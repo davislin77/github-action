b8f84842e264c393db518bcb431fff6ae1dfb9d9
Davis Lin <80668838+davislin77@users.noreply.github.com>
2023-12-14 16:47:29 +0800

    🎮 test: test nok

--- src/util.test.js
+++ src/util.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { sum, mult } from './util'
+import { sum, mult, minus } from './util'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
@@ -7,5 +7,9 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('multi 2 x 3 to equal 6', () => {
   expect(mult(2, 3)).toBe(6)
+})
+
+test('minus 5 - 2 to equal 3', () => {
+  expect(minus(5, 2)).toBe(3)
 })
 
